b4eff23d3e5e7046e3f42b064cbc78e7f7c12b39
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2024-08-02 11:22:57 +0700

    ok

--- api/index.py
+++ api/index.py
@@ -6,4 +6,4 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(port=5328)
+    app.run(port=3000, debug=True)
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+                destination: 'http://127.0.0.1:3000/:path*', // Proxy to Backend
             },
         ];
     },
